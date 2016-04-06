b436aa48b176af5057dc9e7d3daaea99879632b4
ShinLinh <100017631@student.swin.edu.au>
2016-04-07 08:55:34 +1000

    Setup basic classes for the first sprint

---
+++ DP2Project/Inventory Managemnt/Inventory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventoryManagemnt
+{
+    List<item> fInventory = new List<item>;
+    class Inventory
+    {
+        
+    }
+}
---
+++ DP2Project/Inventory Managemnt/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_Managemnt
+{
+    class Item
+    {
+        int _id;
+        string _itemName;
+        float _price;
+        int _inStock;
+    }
+}
